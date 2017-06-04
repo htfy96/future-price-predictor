d975d23d4a1911aa801bac693e6c25c91d0df6ed
Vic Luo <vicluo96@gmail.com>
2017-06-04 20:11:04 +0800

    rnn: allow user to customize layer number and hidden state

--- run_rnn.py
+++ run_rnn.py
@@ -6,21 +6,24 @@ from torch.utils.data.sampler import RandomSampler
 import argparse
 import torch
 
-
 parser = argparse.ArgumentParser(description='RNN Future predictor')
-parser.add_argument('--data', type=str, default='./processed/m0000.h5', help='location of DB file')
+parser.add_argument('--data', type=str, default='./processed/v2-m0000.h5', help='location of DB file')
 parser.add_argument('--cuda', action='store_true', help='whether use cuda')
 parser.add_argument('--rec_url', type=str, default='orzserver.intmainreturn0.com', help='TensorBoard address')
 parser.add_argument('--name', type=str, default='', help='Name of this experiment')
 parser.add_argument('--read_first_k', type=int, default=-1, help='Only read first k table')
 parser.add_argument('--read_old_model', type=str, default=None, help='Path of old model')
-parser.add_argument('--gpu', type=int, default=2, help='Number of gpu')
+parser.add_argument('--gpu', type=int, default=3, help='Number of gpu')
+parser.add_argument('--normalize', type=bool, default=False, help='Whether to perform per-table normalize')
+parser.add_argument('--layer', type=int, default=2, help='Number of RNN layer')
+parser.add_argument('--hidden_state', type=int, default=96, help='Number of hidden state')
 args = parser.parse_args()
 
-with DBGeneticReader(args.data, read_first_k_table=args.read_first_k, two_class=True) as reader:
+with DBGeneticReader(args.data, read_first_k_table=args.read_first_k, two_class=True,
+                     normalize=args.normalize) as reader:
     with torch.cuda.device(args.gpu):
         if args.read_old_model is None:
-            model = rnn.RNNModel(64, rnn_len=20, hidden_state=32)
+            model = rnn.RNNModel(64, rnn_len=args.layer, hidden_state=args.hidden_state)
             if args.cuda:
                 model.cuda()
         else:
@@ -28,5 +31,5 @@ with DBGeneticReader(args.data, read_first_k_table=args.read_first_k, two_class=
 
         cc = CrayonClient(hostname=args.rec_url)
         exp = cc.create_experiment('Future_{}_{}'.format(args.name,
-                                                     datetime.datetime.now().strftime("%I:%M%p on %B %d, %Y")))
+                                                         datetime.datetime.now().strftime("%I:%M%p on %B %d, %Y")))
         rnn.train(model, reader, exp, args, use_cuda=args.cuda)
