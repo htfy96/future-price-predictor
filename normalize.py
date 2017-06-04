543cbaff4ee8089a1b6fd5deaaf8c949bb1fd9b5
Vic Luo <vicluo96@gmail.com>
2017-06-04 16:39:40 +0800

    Normalizer: implement v2 normalizer

--- normalize.py
+++ normalize.py
@@ -1,28 +1,61 @@
 import pandas as pd
 import os
 import math
+import argparse
+import numpy as np
 
-for root, dirs, files in os.walk('./processed'):
-    for filename in files:
-        if filename.endswith('.h5'):
-            fullpath = os.path.join(root, filename)
-            print('=== Normalize h5 file {}'.format(fullpath))
-            with pd.HDFStore(fullpath, complevel=9, complib='blosc') as store:
-                for k in store:
-                    print(' -- Processing table {}'.format(k))
-                    t = store[k]
-                    t = t[t['AskVolume1'] < 100000][t['Turnover'] >= 0][t['LastTurnover'] >= 0]
-                    s_min = t.min()
-                    s_max = t.max()
-                    if s_max['Turnover'] > 10000:
-                        print('  - Normalizing columns')
-                        t[['Turnover', 'LastTurnover']] = t[['Turnover', 'LastTurnover']].applymap(
-                            lambda x: math.log(x+1))
-                        s_min = t.min()
-                        s_max = t.max()
+parser = argparse.ArgumentParser(description='Normalizer')
+parser.add_argument('--dir', type=str, default='./processed', help='Iterate under the directory')
+parser.add_argument('--file', type=str, default='', help='Normalize this file')
+args = parser.parse_args()
 
-                    for col in t.columns[4:]:
-                        print('    -- processing col {} min={} max={}'.format(col, s_min[col], s_max[col]))
-                        if s_min[col] != s_max[col]:
-                            t[col] = (t[col] - s_min[col]) / (s_max[col] - s_min[col])
-                    store[k] = t
+
+def normalize_file(path):
+    print('=== Normalize h5 file {}'.format(path))
+    with pd.HDFStore(path, complevel=9, complib='blosc') as store:
+        tables = []
+        for k in store:
+            print(' -- Processing table {}'.format(k))
+
+            fill_table = {
+                'AskPrice1': ['AskPrice2', 'AskPrice3', 'AskPrice4', 'AskPrice5'],
+                'BidPrice1': ['BidPrice2', 'BidPrice3', 'BidPrice4', 'BidPrice5'],
+                'AskVolume1': ['AskVolume2', 'AskVolume3', 'AskVolume4', 'AskVolume5'],
+                'BidVolume1': ['BidVolume2', 'BidVolume3', 'BidVolume4', 'BidVolume5']
+            }
+
+            for source in fill_table:
+                for target in fill_table[source]:
+                    store[k][target] = np.where(store[k][target] == 0, store[k][source], store[k][target])
+
+            t = store[k]
+            t = t[t['AskVolume1'] < 100000][t['Turnover'] >= 0][t['LastTurnover'] >= 0]
+            t[['Turnover', 'LastTurnover']] = t[['Turnover', 'LastTurnover']].applymap(
+                lambda x: math.log(x + 1))
+
+            tables.append(t.iloc[:, 4:].astype('float32'))
+
+        big_table = pd.concat(tables, copy=False)
+        mean = big_table.mean()
+        std = big_table.std()
+
+        print('Mean=')
+        print(mean)
+
+        print('Std=')
+        print(std)
+        for k in store:
+            print(' -- Writing back to table {}'.format(k))
+            store[k].iloc[:, 4:] = (store[k].iloc[:, 4:] - mean) / (std + 0.00001)
+
+
+
+
+if args.file != '':
+    for root, dirs, files in os.walk('./processed'):
+        for filename in files:
+            if filename.endswith('.h5'):
+                fullpath = os.path.join(root, filename)
+                normalize_file(fullpath)
+else:
+    normalize_file(args.file)
--- preprocessing.py
+++ preprocessing.py
@@ -1,13 +1,20 @@
 import pandas as pd
 import os
+import argparse
 
-for root, dirs, files in os.walk('data'):
+parser = argparse.ArgumentParser(description='Preprocessor')
+parser.add_argument('--source', type=str, default='./data', help='location of csv files')
+parser.add_argument('--target', type=str, default='./processed', help='location of processed h5 files')
+parser.add_argument('--prefix', type=str, default='v2', help='Prefix added to output name')
+args = parser.parse_args()
+
+for root, dirs, files in os.walk(args.source):
     for filename in files:
         if filename.endswith('.csv'):
             fullpath = os.path.join(root, filename)
             print('Processing data from csv={}'.format(fullpath))
             frame = pd.read_csv(fullpath)
-            output_filename = os.path.join('processed', os.path.split(root)[-1] + '.h5')
+            output_filename = os.path.join(args.target, args.prefix + '-' + os.path.split(root)[-1] + '.h5')
 
             chars_to_replace = ('[', ']', '.', ' ')
             str_to_replace = filename.split('.')[0]
